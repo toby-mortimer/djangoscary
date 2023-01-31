92a8092b221dbc16ca88138ce78946f0989730b2
chloe <csabijon@s6c.mlp.college>
2023-01-31 09:36:01 +0000

    added last name

--- differenttest/admin.py
+++ differenttest/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import NewUser
-admin.site.register(NewUser)
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+class UserAdminConfig(UserAdmin):
+    ordering = ('-start_date',)
+    list_display = ('email', 'user_name', 'first_name', 'last_name', 'is_active')
+
+
+admin.site.register(NewUser, UserAdminConfig)
 
